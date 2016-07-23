3b3c527952c80d9ab3af0236bdf77c3f8307115a
Kalentine Vamenek <aisseday@gmail.com>
2016-07-23 17:23:20 +0300

    Fix script output

--- parse_wp_info.js
+++ parse_wp_info.js
@@ -7,6 +7,10 @@ const EMPTY = {
     desc: ''
 };
 
+const out = obj => {
+    console.log(JSON.stringify(obj));
+};
+
 got(url, {
     headers: {
         'Accept-Language': 'en-US',
@@ -17,16 +21,16 @@ got(url, {
     try {
         const $ = cheerio.load(response.body);
 
-        console.log({
+        out({
             name: $(".b-501px__name").text(),
             desc: $(".b-501px__description").text()
         });
 
     } catch (e) {
-        console.log(EMPTY);
+        out(EMPTY);
     }
 
 
 }).catch(() => {
-    console.log(EMPTY);
+    out(EMPTY);
 });
