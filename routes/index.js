c7ed4e309bf7487108747876515b5bd2d0cdbabc
Kalentine Vamenek <aisseday@gmail.com>
2016-07-24 20:31:53 +0300

    Update conf

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,12 @@ var router = express.Router();
 const DEFAULT_WP_URL = 'http://gettab1.site/wp/wp.png';
 const EXTENSION_URL = 'https://chrome.google.com/webstore/';
 
+const DEFAULT_DESC = "GetTab Chrome Extension";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/extension', function(req, res, next) {
     const wp = req.query.wp || DEFAULT_WP_URL;
-    const desc = req.query.desc;
+    const desc = req.query.desc || DEFAULT_DESC;
 
     res.render('extension', {
         description: desc,
