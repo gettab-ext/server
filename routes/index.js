ab2c504e6f3c33f6bc1b95c1156757f662e58983
Kalentine Vamenek <aisseday@gmail.com>
2016-07-24 16:07:00 +0300

    Extension site

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const DEFAULT_WP_URL = 'http://gettab1.site/wp/wp.png';
+const EXTENSION_URL = 'https://chrome.google.com/webstore/';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/extension', function(req, res, next) {
+    const wp = req.query.wp || DEFAULT_WP_URL;
+    const desc = req.query.desc;
+
+    res.render('extension', {
+        description: desc,
+        wpUrl: wp,
+        extensionUrl: EXTENSION_URL,
+        layout: false
+    });
 });
 
 module.exports = router;
