4e5a3b92fc693bcaa61c27fe8b59bd4f8cf59844
Valentin Kamenek <kalle@yandex-team.ru>
2016-07-26 16:17:23 +0300

    Weather API call

--- routes/api.js
+++ routes/api.js
@@ -9,47 +9,31 @@ var weather = require('../lib/weather');
 
 var router = express.Router();
 
-/**
- * Weather API proxy
- */
-router.get('/w', function (req, res) {
-
-});
-
-/**
- *
- */
-router.get('/g', function (req, res) {
-    const coord = req.query.coord;
-    if (!coord) {
-        res.sendStatus(500);
-    }
-
-});
-
 router.get('/weather', function(req, res) {
     const clientIP = req.headers['x-forwarded-for'];
 
     geolocation.getLocation(clientIP).then(location => {
-
-        res.send({
-            location: {
-                latitude: location.latitude,
-                longitude: location.longitude,
-                city: location.city
-            },
-            now: {
-                feelsLike: '',
-                iconCode: '',
-                windSpeed: '',
-                precipProbability: ''
-            },
-            forecast: [{
-                shortDescription: '',
-                timeLocalStr: '',
-                temperatureLow: '',
-                temperatureHigh: ''
-            }]
+        weather.getWeatherData(location.latitude, location.longitude).then(weatherData => {
+            res.send({
+                location: {
+                    latitude: location.latitude,
+                    longitude: location.longitude,
+                    city: location.city
+                },
+                now: {
+                    feelsLike: '',
+                    iconCode: '',
+                    windSpeed: '',
+                    precipProbability: ''
+                },
+                forecast: [{
+                    shortDescription: '',
+                    timeLocalStr: '',
+                    temperatureLow: '',
+                    temperatureHigh: ''
+                }],
+                weatherData: weatherData
+            });
         });
     });
 
