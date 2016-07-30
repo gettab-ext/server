1e3ae9cb82d9771b34f641b79b70d743640b436d
Kalentine Vamenek <aisseday@gmail.com>
2016-07-30 23:38:40 +0300

    Fix nginx script 2

--- scripts/make_nginx.sh
+++ scripts/make_nginx.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-ln -sf ../etc/gettab1.conf /etc/nginx/sites-enabled/gettab1;
+ln -sf /opt/gettab-server/etc/gettab1.conf /etc/nginx/sites-enabled/gettab1;
 /etc/init.d/nginx reload;
