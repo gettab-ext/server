b5a81e15b98669929543e24f5823d66a624ee524
Valentin Kamenek <kalle@yandex-team.ru>
2016-08-02 14:08:15 +0300

    Fix config

---
+++ scripts/make_balancer.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo apt-get update;
+sudo apt-get install nginx -y;
+
+wget https://raw.githubusercontent.com/gettab-ext/server/master/etc/balancer.conf -O /etc/nginx/sites-enabled/balancer.conf;
+
