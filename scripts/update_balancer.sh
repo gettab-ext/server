431e9af76af5d0d75127d6b0bd4511e784c03a62
Valentin Kamenek <kalle@yandex-team.ru>
2016-08-13 15:33:29 +0300

    nginx balancer tuning

--- scripts/update_balancer.sh
+++ scripts/update_balancer.sh
@@ -1,3 +1,3 @@
-wget https://raw.githubusercontent.com/gettab-ext/server/master/etc/balancer.conf -O /etc/nginx/nginx.conf
+wget https://raw.githubusercontent.com/gettab-ext/server/master/etc/balancer.conf -O /etc/nginx/nginx.conf --no-cache
 
 sudo /etc/init.d/nginx restart;
